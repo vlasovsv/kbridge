92561733651246157759fb82364b27b77b340922
vlasovsv <vlasovsv@gmail.com>
2021-04-19 23:55:11 +0300

    Added parameters for source Kafka: Consumer group and AutoOffsetReset

--- src/KafkaBridge/KafkaSource.cs
+++ src/KafkaBridge/KafkaSource.cs
@@ -6,7 +6,7 @@ namespace KafkaBridge
 {
     public record KafkaSource
     {
-        public KafkaSource(string bootstrapServers, string topic)
+        public KafkaSource(string bootstrapServers, string topic, string groupId, AutoOffsetReset autoOffsetReset)
         {
             BootstrapServers = Guard.Argument(bootstrapServers, nameof(bootstrapServers))
                 .NotNull("BootstrapServers must be set")
@@ -15,18 +15,24 @@ namespace KafkaBridge
             Topic = Guard.Argument(topic, nameof(topic))
                 .NotNull("Topic must be set")
                 .NotEmpty("Topic must be set");
+
+            GroupId = groupId;
+            AutoOffsetReset = autoOffsetReset;
         }
         
         public string BootstrapServers { get; }
 
         public string Topic { get; }
 
+        public string GroupId { get; }
+        public AutoOffsetReset AutoOffsetReset { get; }
+
         public ConsumerConfig GetConsumerConfig()
         {
             var cfg = new ConsumerConfig();
             cfg.BootstrapServers = BootstrapServers;
-            cfg.GroupId = $"kafka-bridge {Guid.NewGuid()}";
-            cfg.AutoOffsetReset = AutoOffsetReset.Earliest;
+            cfg.GroupId = GroupId ?? $"kafka-bridge {Guid.NewGuid()}";
+            cfg.AutoOffsetReset = AutoOffsetReset;
 
             return cfg;
         }
--- src/KafkaBridge/Program.cs
+++ src/KafkaBridge/Program.cs
@@ -13,23 +13,27 @@ namespace KafkaBridge
         /// </summary>
         /// <param name="fromServer">Source kafka bootstrap servers. For example, localhost:9092</param>
         /// <param name="fromTopic">Source kafka topic name</param>
+        /// <param name="groupId">Consumer group id</param>
+        /// <param name="autoOffsetReset">Auto offset reset</param>
         /// <param name="toServer">Destination kafka bootstrap servers. For example, localhost:9092</param>
         /// <param name="toTopic">Destination kafka topic name</param>
         /// 
-        static async Task Main(string fromServer, string fromTopic, string toServer, string toTopic)
+        static void Main(string fromServer, string fromTopic, 
+            string groupId, AutoOffsetReset autoOffsetReset, 
+            string toServer, string toTopic)
         {
             var cts = new CancellationTokenSource();
             var channel = Channel.CreateUnbounded<string>();
 
             fromServer ??= KafkaDefaults.BootstrapServers;
-            var source = new KafkaSource(fromServer, fromTopic);
+            var source = new KafkaSource(fromServer, fromTopic, groupId, autoOffsetReset);
             var destination = new KafkaDestination(toServer ?? fromServer, toTopic ?? fromTopic);
 
             var producerTask = StartProducer(destination.Topic, destination.GetProducerConfig(), channel.Reader,
                 cts.Token);
             var consumerTask = StarConsumer(source.Topic, source.GetConsumerConfig(), channel.Writer, cts.Token);
 
-            Console.WriteLine("Started");
+            Console.WriteLine("Press any key to stop");
             Console.ReadKey();
             cts.Cancel();
         }
@@ -61,7 +65,7 @@ namespace KafkaBridge
                 while (await reader.WaitToReadAsync(cancellationToken))
                 {
                     var msg = await reader.ReadAsync(cancellationToken);
-                    producer.ProduceAsync
+                    await producer.ProduceAsync
                     (
                         topic, new Message<Null, string>()
                         {
